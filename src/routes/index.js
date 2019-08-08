4b89ca0e316ab32b99690d4a1b050acca52b5cd1
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-08 20:52:13 -0300

    :tada: Initial configs

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashborad from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+      <Route path="/dashboard" component={Dashborad} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
