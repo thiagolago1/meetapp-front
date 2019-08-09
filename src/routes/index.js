b4da3dfac7f9b9e5224ad473a54b3f98d6f0ffef
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-09 15:47:01 -0300

    :construction: [ADD] Private routes configs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashborad from '../pages/Dashboard';
@@ -10,8 +11,8 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashborad} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashborad} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
