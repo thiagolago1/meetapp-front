8d86e8ddeccdb3b0a6666488488dce1be5f8b522
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-09 18:00:34 -0300

    :art: [ADD] [200~

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input typo="password" placeholder="Sua senha" />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
