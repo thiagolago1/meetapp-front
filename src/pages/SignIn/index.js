7be0719424791d50c864b824a37c3d71bf90257c
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-12 18:39:47 -0300

    :bug: :construction: [Fix] signIn and SignUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
-import logo from '~/assets/logo.svg';
-import { signInRequest } from '~/store/modules/auth/actions';
 
-const schema = Yup.object().shape({
-  email: Yup.string()
-    .email('Insira um e-mail válido')
-    .required('O e-mail é obrigatório'),
-  password: Yup.string().required('A senha é obrigatória'),
-});
+import { signInRequest } from '../../store/modules/auth/actions';
+import logo from '../../assets/logo.svg';
 
 export default function SignIn() {
-  const dispatch = useDispatch();
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Insira um email válido.')
+      .required('O email é necessário.'),
+    password: Yup.string().required('A senha é necessária.'),
+  });
   const loading = useSelector(state => state.auth.loading);
+  const dispatch = useDispatch();
 
-  function handleSubmit(email, password) {
+  function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
   }
 
   return (
     <>
-      <img src={logo} alt="MeetApp" />
-
+      <img src={logo} alt="MeetApp logo" />
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="email" type="email" placeholder="Seu e-mail" />
-        <Input name="password" type="password" placeholder="Sua senha" />
-        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
-        <Link to="/register">Criar conta gratuita</Link>
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">{loading ? 'Carregando...' : 'Entrar'}</button>
+        <Link to="/register">Criar conta grátis</Link>
       </Form>
     </>
   );
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,37 +1,42 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
-import logo from '~/assets/logo.svg';
-import { signUpRequest } from '~/store/modules/auth/actions';
 
-const schema = Yup.object().shape({
-  name: Yup.string().required('Insira seu nome'),
-  email: Yup.string()
-    .email('Insira um e-mail válido')
-    .required('O e-mail é obrigatório'),
-  password: Yup.string()
-    .min(6, 'Mínimo de 6 caractéres')
-    .required('A senha é obrigatória'),
-});
+import { signUpRequest } from '../../store/modules/auth/actions';
+import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é necessário.'),
+    email: Yup.string()
+      .email('Insira um email válido.')
+      .required('O email é necessário.'),
+    password: Yup.string().required('A senha é necessária.'),
+  });
+  const loading = useSelector(state => state.auth.loading);
   const dispatch = useDispatch();
 
-  function handleSubmit(name, email, password) {
+  function handleSubmit({ name, email, password }) {
     dispatch(signUpRequest(name, email, password));
   }
+
   return (
     <>
-      <img src={logo} alt="MeetApp" />
-
+      <img src={logo} alt="MeetApp logo" />
       <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
-        <Input name="email" type="email" placeholder="Seu e-mail" />
-        <Input name="password" type="password" placeholder="Sua senha" />
-        <button type="submit">Criar conta</button>
-        <Link to="/">Já possuo login</Link>
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Criar conta'}
+        </button>
+        <Link to="/">Já tenho login</Link>
       </Form>
     </>
   );
